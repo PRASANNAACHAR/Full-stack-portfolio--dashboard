c11608e20fc6bb31f87f7d72320de9a4d23b4796
Prasanna Achar <prasannaachar64@gmail.com>
2025-08-07 21:29:39 +0530

    profile

--- src/pages/sub-components/Profile.jsx
+++ src/pages/sub-components/Profile.jsx
@@ -27,16 +27,16 @@ const Profile = () => {
                        <Label >Profile Image</Label>
                        <img src={user && user.avatar && user.avatar.url} alt="avatar" className='w-full h-auto sm:w-72 sm:h-72 rounded-2xl' />
                     </div>
-                                {user && user.resume && user.resume.url && (
-                                     <div className='grid gap-2 w-full sm:w-72'>
-                                          <Label>Resume</Label>
-                                          <img
-                                               src={user.resume.url}
-                                               alt="resume"
-                                               className='w-full h-auto sm:w-72 sm:h-72 rounded-2xl'
-                                          />
-                                     </div>
-                                )}
+                    {user?.resume?.url && (
+  <div className='grid gap-2 w-full sm:w-72'>
+    <Label>Resume</Label>
+    <iframe
+      src={user.resume.url}
+      title="resume"
+      className='w-full h-72 sm:w-72 sm:h-72 rounded-2xl border'
+    />
+  </div>
+)}
 
                 </div>
                 <div className='grid gap-2'>
